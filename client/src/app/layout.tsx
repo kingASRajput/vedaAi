ed10f2c767c8e2ac9544b0d24b9b874c1e32b957
Anmol Singh Rajput <37495826+kingASRajput@users.noreply.github.com>
2026-04-06 22:04:07 +0530

    Fix SacredParticles prop

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen">
-        <SacredParticles count={40} />
+        <SacredParticles />
         <OmBackground />
         <Navbar />
         <main className="pt-16 relative z-10">
